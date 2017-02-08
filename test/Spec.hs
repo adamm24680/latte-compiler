78f2c079524005b2d12ecfe1fb14b79d53913883
Adam Markiewicz <adamm24680@gmail.com>
2017-02-08 16:59:06 +0100

    tests?

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,26 @@
+import System.Process(callCommand)
+import Text.Printf
+import System.Directory
+import Control.Monad
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = core
+
+core :: IO ()
+core = forM_ [1..18] (testCase "lattests/good/core")
+
+testCase :: String -> Int -> IO ()
+testCase name i = do
+  let name = printf "%s%03d" name i
+  let inputName = name ++ ".input"
+  let latFileName = name ++ ".lat"
+  let outputName = name ++ ".output"
+  inputExists <- doesFileExist inputName
+  callCommand $ "stack exec latc-exe " ++ latFileName
+  if inputExists then
+    callCommand $ printf "cat %s | %s > out" inputName name
+  else
+    callCommand $ printf "%s > out" name
+  in1 <- readFile "out"
+  in2 <- readFile outputName
+  unless (in1 == in2) $ print ("test " ++ show i ++ " failed")
