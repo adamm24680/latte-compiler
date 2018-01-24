43736a733d61a143e7fc31d0e7d7d1edcc1b4013
adamm <adamm24680@gmail.com>
2018-01-24 11:15:15 +0100

    test error messages

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,12 +2,16 @@ import System.Process(callCommand)
 import Text.Printf
 import System.Directory
 import Control.Monad
+import Control.Exception
 
 main :: IO ()
-main = core
+main = core >> bad
 
 core :: IO ()
 core = forM_ [1..22] (testCase "lattests/good/core")
+
+bad :: IO ()
+bad = forM_ [1..27] (testBad "lattests/bad/bad")
 
 testCase :: String -> Int -> IO ()
 testCase base i = do
@@ -24,3 +28,10 @@ testCase base i = do
   in1 <- readFile "out"
   in2 <- readFile outputName
   unless (in1 == in2) $ putStrLn ("test " ++ show i ++ " failed\n"++ (unlines $ map show $ zip (lines in1) (lines in2)))
+
+testBad :: String -> Int -> IO ()
+testBad base i = do
+  let name = printf "%s%03d" base i
+  let latFileName = name ++ ".lat"
+  (try :: IO a -> IO (Either IOException a)) (callCommand $ "stack exec latc_x86 " ++ latFileName)
+  return ()
